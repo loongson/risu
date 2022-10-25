665b4c5c004792e28d7e23702b014cdf455aeded
Song Gao <gaosong@loongson.cn>
2022-10-25 17:39:51 +0800

    loongarch: add complie.sh
    complie.sh includes:
    - build risu;
    - rand insns;
    - run master;
    - run client;
    - dump insns.
    
    Signed-off-by: Song Gao <gaosong@loongson.cn>

---
+++ compile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm -rf build
+mkdir build
+cd build
+ARCH=loongarch64 ../configure --static
+#ARCH=loongarch64 ../configure
+make
+
+# rand insn: 
+# ./risugen --numinsn 100000 loongarch64.risu test.bin
+
+# master : 
+# ./build/risu --master test.bin
+
+# client
+# [./qemu-loongarch ] ./build/risu  --host ip  test.bin
+
+# dump test.bin:
+#  objdump -D -b binary -m loongarch64 gs.bin 2>&1 | tee 1.txt
